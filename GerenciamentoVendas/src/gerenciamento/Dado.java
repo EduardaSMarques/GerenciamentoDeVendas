7fff06d7b87304e3c7566610e45326374d50a7ad
EduardaSMarques <eduardamarques1307@gmail.com>
2021-09-04 02:22:36 -0300

    Atualização da classe Dado

--- GerenciamentoVendas/src/gerenciamento/Dado.java
+++ GerenciamentoVendas/src/gerenciamento/Dado.java
@@ -1,5 +1,31 @@
 package gerenciamento;
 
 public class Dado {
-
+	 
+	Capa[] capasCadastradas = new Capa[20];
+	
+	Carregador[] carregadoresCadastrados = new Carregador[20];
+	
+	Cliente[] clientesCadastrados = new Cliente[20];
+	
+	Conta[] contasCadastradas = new Conta[20];
+	
+	Estoque[] estoquesCadastrados = new Estoque[20];
+	
+	Fone[] fonesCadastrados = new Fone[20];
+	
+	Fornecedor[] fornecedoresCadastrados = new Fornecedor[20];
+	
+	Funcionario[] funcionariosCadastrados = new Funcionario[20];
+	
+	//confirmar se precisa do array de grafico
+	
+	Pelicula[] peliculasCadastradas = new Pelicula[20];
+	
+	//confirmar se precisa do array de Produto
+	
+	Suporte[] suportesCadastrados = new Suporte[20];
+	
+	Venda[] vendasCadastradas = new Venda[20];
+	
 }
