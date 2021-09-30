f22c4d94c3389fc9ef8a66bd9edfe1b4def5fc69
Madu01 <m4dud01@gmail.com>
2021-09-30 19:04:52 -0300

    Menu finalizado com imagem de fundo e botoes estilizados

--- GerenciamentoVendas/src/visualiza/MenuPrincipal.java
+++ GerenciamentoVendas/src/visualiza/MenuPrincipal.java
@@ -4,93 +4,105 @@ import controle.*;
 
 import javax.swing.*;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.*;
 
 public class MenuPrincipal implements ActionListener{
 
-	private JFrame janela = new JFrame("Gerenciamento de Vendas");
-	private static JButton vendas = new JButton("");
-	private JLabel vend = new JLabel("Vendas");
-	private static JButton produtos = new JButton("");
-	private JLabel prod = new JLabel("Produtos");
-	private static JButton clientes = new JButton("");
-	private JLabel client = new JLabel("Clientes");
-	private static JButton funcionarios = new JButton("");
-	private JLabel fun = new JLabel("Funcionários");
-	private static JButton estoque = new JButton("");
-	private JLabel est = new JLabel("Estoque");
+	private static JFrame janelaMenu = new JFrame("Gerenciamento de Vendas");
+	private static JLabel fundo = new JLabel("");
+	private static JButton btnVendas = new JButton("Vendas");
+	private static JButton btnProdutos = new JButton("Produtos");
+	private static JButton btnClientes = new JButton("Clientes");
+	private static JButton btnFuncionarios = new JButton("Funcionários");
+	private static JButton btneEtoque = new JButton("Estoque");
 	public static ControleDado dads = new ControleDado();
 	
 	public MenuPrincipal() {
-		vendas.setIcon(new ImageIcon("imgFundo.jpeg"));
 		
-		vend.setFont(new Font("Microsoft Sans Serif", Font.BOLD, 40));
-		prod.setFont(new Font("Microsoft Sans Serif", Font.BOLD, 40));
-		client.setFont(new Font("Microsoft Sans Serif", Font.BOLD, 40));
-		fun.setFont(new Font("Microsoft Sans Serif", Font.BOLD, 40));
-		est.setFont(new Font("Microsoft Sans Serif", Font.BOLD, 40));
+		fundo.setIcon(new ImageIcon(MenuPrincipal.class.getResource("/imagem/imgFundo.jpeg")));
 		
-		vend.setBounds(116, 294, 188, 52);
-		prod.setBounds(422, 294, 188, 52);
-		client.setBounds(749, 294, 179, 52);
-		fun.setBounds(216, 621, 262, 52);
-		est.setBounds(619, 621, 158, 52);
+		btnVendas.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 43));
+		btnVendas.setBackground(Color.DARK_GRAY);
+		btnVendas.setForeground(Color.WHITE);
 		
-		vendas.setBounds(73, 110, 214, 166);
-		produtos.setBounds(396, 110, 214, 166);
-		clientes.setBounds(726, 110, 214, 166);
-		funcionarios.setBounds(228, 432, 214, 166);
-		estoque.setBounds(582, 432, 214, 166);
+		btnProdutos.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 43));
+		btnProdutos.setBackground(Color.DARK_GRAY);
+		btnProdutos.setForeground(Color.WHITE);
 		
-		janela.setLayout(null);
+		btnClientes.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 43));
+		btnClientes.setBackground(Color.DARK_GRAY);
+		btnClientes.setForeground(Color.WHITE);
 		
-		janela.add(vend);
-		janela.add(prod);
-		janela.add(client);
-		janela.add(fun);
-		janela.add(est);
-		janela.add(vendas);
-		janela.add(produtos);
-		janela.add(clientes);
-		janela.add(funcionarios);
-		janela.add(estoque);
+		btnFuncionarios.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 43));
+		btnFuncionarios.setBackground(Color.DARK_GRAY);
+		btnFuncionarios.setForeground(Color.WHITE);
 		
-		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		janela.setBounds(100, 100, 1012, 744);
-		janela.setLocationRelativeTo(null); //para janela ficar centralizada na hora de abrir
-		janela.setVisible(true);
+		btneEtoque.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 43));
+		btneEtoque.setBackground(Color.DARK_GRAY);
+		btneEtoque.setForeground(Color.WHITE);
+		
+		btnVendas.setBounds(78, 177, 214, 99);
+		btnProdutos.setBounds(396, 177, 214, 99);
+		btnClientes.setBounds(726, 177, 214, 99);
+		btnFuncionarios.setBounds(188, 441, 302, 106);
+		btneEtoque.setBounds(582, 441, 214, 106);
+		fundo.setBounds(0, 0, 1025, 707);
+		
+		janelaMenu.setLayout(null);
+		
+		janelaMenu.add(btnVendas);
+		janelaMenu.add(btnProdutos);
+		janelaMenu.add(btnClientes);
+		janelaMenu.add(btnFuncionarios);
+		janelaMenu.add(btneEtoque);
+		janelaMenu.add(fundo);
+		
+		janelaMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janelaMenu.setBounds(100, 100, 1012, 744);
+		janelaMenu.setLocationRelativeTo(null); //para janela ficar centralizada na hora de abrir
+		janelaMenu.setVisible(true);
 	}
 	
 	public static void main(String[] args) {
 		MenuPrincipal menuPrinc = new MenuPrincipal();
 		
-		vendas.addActionListener(menuPrinc);
-		produtos.addActionListener(menuPrinc);
-		clientes.addActionListener(menuPrinc);
-		funcionarios.addActionListener(menuPrinc);
-		estoque.addActionListener(menuPrinc);
+		btnVendas.addActionListener(menuPrinc);
+		btnProdutos.addActionListener(menuPrinc);
+		btnClientes.addActionListener(menuPrinc);
+		btnFuncionarios.addActionListener(menuPrinc);
+		btneEtoque.addActionListener(menuPrinc);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
 		Object src = e.getSource();
 
-		if (src == vendas) {
+		if (src == btnVendas) {
 			
 		}
-		if (src == produtos) {
-			new JanelaProduto().mostraTelaProd();
+		
+		if (src == btnProdutos) {
+			msgProdutoEmManuntencao();
 		}
-		if (src == clientes) {
-
+		
+		if (src == btnClientes) {
 		}
-		if (src == funcionarios) {
+		
+		if (src == btnFuncionarios) {
 			new JanelaFuncionario().mostraTelaFuncio(dads);
 		}		
 		
-		if (src == estoque) {
+		if (src == btneEtoque) {
 			new JanelaEstoque().mostraTelaEstoq(dads);
 		}
 	}
 
+	public void msgProdutoEmManuntencao() {
+		JOptionPane.showMessageDialog(null, 
+				"                Tela produto em manutenção!\n"
+				+ "as funcionalidades ainda precisam serem efetuadas.\n", null, 
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+	
 }
